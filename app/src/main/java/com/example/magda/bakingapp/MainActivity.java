2b8999c1d4fcd209b3a7809198d318220107c5e2
magda <magdalena.noffke@googlemail.com>
2017-07-25 21:38:08 +0200

    use different layout file for different screen sizes

--- app/src/main/java/com/example/magda/bakingapp/MainActivity.java
+++ app/src/main/java/com/example/magda/bakingapp/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.magda.bakingapp;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Parcelable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -21,18 +23,57 @@ import retrofit2.Call;
 
 public class MainActivity extends AppCompatActivity implements ReceipeAdapter.ReceipeAdapterOnClickHandler {
 
+    private boolean mTwoPane;
+    private ReceipeDetailFragment detailFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if(findViewById(R.id.baking_app_linear_layout) != null) {
+            mTwoPane = true;
+
+            ReceipeDetailFragment detailFragment = new ReceipeDetailFragment();
+            //detailFragment.setmReceipe(mReceipe);
+
+            FragmentManager fragmentManager = getSupportFragmentManager();
+
+            fragmentManager.beginTransaction()
+                    .add(R.id.receipe_detail_fragment_container, detailFragment)
+                    .commit();
+        } else {
+            mTwoPane = false;
+        }
     }
 
     @Override
     public void onReceipeClick(Receipe receipe) {
-        Bundle b = new Bundle();
-        b.putParcelable("RECEIPE", receipe);
-        final Intent intent = new Intent(this, ReceipeDetailActivity.class);
-        intent.putExtras(b);
-        startActivity(intent);
+        if(mTwoPane == false) {
+            Bundle b = new Bundle();
+            b.putParcelable("RECEIPE", receipe);
+            final Intent intent = new Intent(this, ReceipeDetailActivity.class);
+            intent.putExtras(b);
+            startActivity(intent);
+        } else {
+            ReceipeDetailFragment newFragment = new ReceipeDetailFragment();
+            newFragment.setmReceipe(receipe);
+
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.receipe_detail_fragment_container,newFragment)
+                    .commit();
+        }
+    }
+
+    public boolean ismTwoPane() {
+        return mTwoPane;
+    }
+
+    public ReceipeDetailFragment getDetailFragment() {
+        return detailFragment;
+    }
+
+    public void setDetailFragment(ReceipeDetailFragment detailFragment) {
+        this.detailFragment = detailFragment;
     }
 }
--- app/src/main/java/com/example/magda/bakingapp/MasterListFragment.java
+++ app/src/main/java/com/example/magda/bakingapp/MasterListFragment.java
@@ -4,6 +4,7 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -70,7 +71,6 @@ public class MasterListFragment extends Fragment {
         protected void onPostExecute(List<Receipe> receipiesData) {
             if (receipiesData != null) {
                 mReceipeAdapter.setmRecepies((ArrayList<Receipe>) receipiesData);
-
             }
         }
 
