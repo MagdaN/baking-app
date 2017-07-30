4f9d177914ee6f0c1aef6346c283ceed15f480f0
magda <magdalena.noffke@googlemail.com>
2017-07-30 21:38:30 +0200

    display video

--- app/src/main/java/com/example/magda/bakingapp/StepActivity.java
+++ app/src/main/java/com/example/magda/bakingapp/StepActivity.java
@@ -2,13 +2,53 @@ package com.example.magda.bakingapp;
 
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
+import android.support.v4.media.session.MediaSessionCompat;
+import android.support.v4.media.session.PlaybackStateCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
-public class StepActivity extends AppCompatActivity {
+import com.example.magda.bakingapp.models.Step;
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.ExoPlaybackException;
+import com.google.android.exoplayer2.ExoPlayer;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.LoadControl;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.Timeline;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.TrackGroupArray;
+import com.google.android.exoplayer2.trackselection.AdaptiveVideoTrackSelection;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelection;
+import com.google.android.exoplayer2.trackselection.TrackSelectionArray;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+
+import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
+
+public class StepActivity extends AppCompatActivity implements ExoPlayer.EventListener {
+
+    private Step mStep;
+    private TextView mReceipeName;
+    private TextView mStepId;
+    private TextView mStepShortDescription;
+    private TextView mStepDescription;
+    private SimpleExoPlayerView mExoPlayerView;
+    private SimpleExoPlayer mExoPlayer;
+    private MediaSessionCompat mMediaSession;
+    private static final String TAG = StepActivity.class.getSimpleName();
+    private PlaybackStateCompat.Builder mStateBuilder;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -19,13 +59,150 @@ public class StepActivity extends AppCompatActivity {
 
         if (intentThatStartedThisActivity != null) {
             if (intentThatStartedThisActivity.hasExtra("step")) {
+                mStep = intentThatStartedThisActivity.getParcelableExtra("step");
 
+                mStepId = (TextView) findViewById(R.id.step_view_id);
+                mStepId.setText(Integer.toString(mStep.getmId()));
+
+                String description = mStep.getMshortDescription();
+
+                mStepShortDescription = (TextView) findViewById(R.id.step_view_short_description);
+                mStepShortDescription.setText(mStep.getMshortDescription());
+
+                mStepDescription = (TextView) findViewById(R.id.step_view_description);
+                mStepDescription.setText(mStep.getmDescription());
+
+                mExoPlayerView = (SimpleExoPlayerView) findViewById(R.id.step_video);
+
+                String videoUrl = mStep.getmVideoURL();
+
+                if(videoUrl != null && !videoUrl.isEmpty()) {
+                    initializeMediaSession();
+                    initializePlayer(Uri.parse(mStep.getmVideoURL()));
+                } else {
+                    mExoPlayerView.setVisibility(View.GONE);
+                }
             }
             if (intentThatStartedThisActivity.hasExtra("receipe_name")) {
-                TextView recepeiName = (TextView) findViewById(R.id.step_receipe_name);
-                recepeiName.setText(intentThatStartedThisActivity.getStringExtra("receipe_name"));
+                mReceipeName = (TextView) findViewById(R.id.step_receipe_name);
+                mReceipeName.setText(intentThatStartedThisActivity.getStringExtra("receipe_name"));
             }
 
+        }
+    }
+
+    private void initializeMediaSession() {
+        mMediaSession = new MediaSessionCompat(this, TAG);
+        mMediaSession.setFlags(
+                MediaSessionCompat.FLAG_HANDLES_MEDIA_BUTTONS |
+                        MediaSessionCompat.FLAG_HANDLES_TRANSPORT_CONTROLS);
+        mMediaSession.setMediaButtonReceiver(null);
+
+        mStateBuilder = new PlaybackStateCompat.Builder()
+                .setActions(
+                        PlaybackStateCompat.ACTION_PLAY |
+                                PlaybackStateCompat.ACTION_PAUSE |
+                                PlaybackStateCompat.ACTION_SKIP_TO_PREVIOUS |
+                                PlaybackStateCompat.ACTION_PLAY_PAUSE);
+
+        mMediaSession.setPlaybackState(mStateBuilder.build());
+        mMediaSession.setCallback(new MySessionCallback());
+        mMediaSession.setActive(true);
+    }
+
+    private void initializePlayer(Uri mediaUri) {
+        if (mExoPlayer == null) {
+            String userAgent = "Android-ExoPlayer";
+            MediaSource mediaSource = new ExtractorMediaSource(
+                    mediaUri,
+                    new DefaultDataSourceFactory(this, Util.getUserAgent(this, userAgent)),
+                    new DefaultExtractorsFactory(),
+                    null,
+                    null
+            );
+
+            TrackSelector trackSelector = new DefaultTrackSelector();
+            LoadControl loadControl = new DefaultLoadControl();
+            mExoPlayer = ExoPlayerFactory.newSimpleInstance(this, trackSelector, loadControl);
+            mExoPlayerView.setPlayer(mExoPlayer);
+
+            mExoPlayer.prepare(mediaSource);
+            mExoPlayer.setPlayWhenReady(true);
+        }
+
+    }
+
+    private void releasePlayer() {
+        if(mExoPlayer != null) {
+            mExoPlayer.stop();
+            mExoPlayer.release();
+            mExoPlayer = null;
+        }
+
+        if (mMediaSession != null) {
+            mMediaSession.setActive(false);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        releasePlayer();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        releasePlayer();
+    }
+
+    @Override
+    public void onTimelineChanged(Timeline timeline, Object manifest) {
+    }
+
+    @Override
+    public void onTracksChanged(TrackGroupArray trackGroups, TrackSelectionArray trackSelections) {
+    }
+
+    @Override
+    public void onLoadingChanged(boolean isLoading) {
+    }
+
+    @Override
+    public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
+        if((playbackState == ExoPlayer.STATE_READY) && playWhenReady){
+            mStateBuilder.setState(PlaybackStateCompat.STATE_PLAYING,
+                    mExoPlayer.getCurrentPosition(), 1f);
+        } else if((playbackState == ExoPlayer.STATE_READY)){
+            mStateBuilder.setState(PlaybackStateCompat.STATE_PAUSED,
+                    mExoPlayer.getCurrentPosition(), 1f);
+        }
+        mMediaSession.setPlaybackState(mStateBuilder.build());
+    }
+
+    @Override
+    public void onPlayerError(ExoPlaybackException error) {
+    }
+
+    @Override
+    public void onPositionDiscontinuity() {
+    }
+
+    private class MySessionCallback extends MediaSessionCompat.Callback {
+
+        @Override
+        public void onPlay() {
+            mExoPlayer.setPlayWhenReady(true);
+        }
+
+        @Override
+        public void onPause() {
+            mExoPlayer.setPlayWhenReady(false);
+        }
+
+        @Override
+        public void onSkipToPrevious() {
+            mExoPlayer.seekTo(0);
         }
     }
 }
